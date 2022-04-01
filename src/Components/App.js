2dc14adeee3664875ba7271b3e1d2f33b7221f99
seanclaybaugh <sdclaybaugh@gmail.com>
2022-04-01 11:19:56 -0700

    completed board and context setup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, createContext }from 'react';
 import '../styles.css';
+import Board from './Board';
+import Keyboard from './Keyboard';
+import { boardDefault } from '../Words';
+export const AppContext = createContext();
+
 
 const App = () => {
-
-  return <h1>Wordle</h1>
-
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle Clone</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
