33ea9769daaa5a1f56f31210a14980d937eb3e05
seanclaybaugh <sdclaybaugh@gmail.com>
2022-04-01 13:44:54 -0700

    keyboard functionality complete

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,16 +7,49 @@ export const AppContext = createContext();
 
 
 const App = () => {
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
   const [board, setBoard] = useState(boardDefault);
+
+
+  const onSelectLetter = (keyVal) => {
+     if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board]
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard)
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+    }
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({
+      ...currAttempt,
+      letterPos: currAttempt.letterPos -1,
+    });
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+  }
+
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle Clone</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onEnter, onDelete, onSelectLetter }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
+
     </div>
   );
 }
--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './App'
+
 
 function Key({keyVal, bigKey}) {
-  return <div className="key" id={bigKey && "big"}>{keyVal}</div>;
+  const { board, setBoard, currAttempt, setCurrAttempt, onEnter, onDelete, onSelectLetter } = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (keyVal === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  }
+
+  return (
+  <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+    {keyVal}
+  </div>
+  )
 }
 
 export default Key
